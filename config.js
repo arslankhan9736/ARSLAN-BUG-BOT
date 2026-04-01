8528c1d1081b7ccbeee1da0b38e8ebaa1aba7cce
arslankhan9736 <arslanahmad9736@gmail.com>
2026-04-01 15:04:21 +0500

    config.js

--- config.js
+++ config.js
@@ -17,8 +17,8 @@
  * YouTube   : https://youtube.com/@voidsec7718
  * Instagram : sabir._7718
  * Telegram  : https://t.me/SABIR7718
- * GitHub    : https://github.com/SABIR7718
- * WhatsApp  : +91 73650 85213
+ * GitHub    : https://github.com/arslankhan9736
+ * WhatsApp  : +923317152423
  *
  * Violations may result in DMCA takedown
  * or termination of the Telegram bot.
@@ -26,7 +26,7 @@
 module.exports = {
     mainToken: 'BOT_TOKEN', // Bot Token Here 
     
-    S7: '@SeXyxeon13', // Owner, Name
+    S7: 'Arslan khan', // Owner, Name
     
     adminId: 'CHAT_ID', // Owner Chat ID ( control /address, /delresell )
     
@@ -36,15 +36,15 @@ module.exports = {
     
     youtube: 'YOITUBE_LINK', // YouTube Link
     
-    waChannel: 'WA_LINK', // WhatsApp Channel Link
+    waChannel: 'WA_LINK', // https://whatsapp.com/channel/0029Vb7etc9Bfxo3aBvjor1O
     
     instagram: 'INSTA_LINK', // Instagram Link 
     
     channelId: 'CHANNEL_ID', // channel ID bot must be admin in the channel
     
-    groupId: 'GROUP_ID', // group ID bot must be admin in the group
+    groupId: 'GROUP_ID', // https://chat.whatsapp.com/Ked6ISwuIJeHCjnsRbqtkK?mode=gi_t
     
-    bot: 'Xeon Bug Bot V27', // Bot name 
+    bot: 'Arsaln bug bot', // Bot name 
     
-    logo: './SY/Loves.jpg' // Bot logo path or URL
+    logo: './SY/Loves.jpg' // https://files.catbox.moe/gqww41.jpg
 };
